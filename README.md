e8787b0e31cd57bc6c067564f024d86cdc635035
dchoi294 <dchoi294@gmail.com>
2022-12-20 05:17:31 +0000

    lab 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# LAB - Class 01
+
+Project: Intro to Python
+
+## Author
+
+- Don Choi
+
+## Description
+
+- A menu shows on and able to order on the terminal.
---
+++ snakes-cafe/snakes_cafe.py
@@ -0,0 +1,63 @@
+menu = """
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+***********************************
+** What would you like to order? **
+***********************************
+"""
+order = {
+    "wings": 0,
+    "cookies": 0,
+    "spring rolls": 0,
+    "salmon": 0,
+    "steak": 0,
+    "meat tornado": 0,
+    "a literal garden": 0,
+    "ice cream": 0,
+    "cake": 0,
+    "pie": 0,
+    "coffee": 0,
+    "tea": 0,
+    "unicorn tears": 0
+}
+
+print(menu)
+while ask_again:
+    user_input = input("> ").lower()
+    if user_input == "quit":
+        ask_again = False
+        break
+    if user_input.lower() in order.keys():
+        order[user_input] += 1
+        orders_text = "order(s)"
+        print(f"** {order[user_input]} {orders_text} of {user_input} have been added to your meal **")
+    else:
+        print("This item is not on the menu.")
+
+if __name__ == "__snake-cafe__":
+    print(__name__)
